4f00ca138273fb76b19f0d1c20c4a4f00147cb9f
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-05-14 14:37:34 +0530

    Fixed all design issue

--- resources/views/partials/sleep_mode.blade.php
+++ resources/views/partials/sleep_mode.blade.php
@@ -1,11 +1,32 @@
-
-<div id="timeout-screen" style="display: none;">
-    
-  <div id="timeout-message">You have been inactive. You will be logged out in <span id="countdown"></span> seconds.</div>
-  <div>
-    <a class="btn btn-regular btn-xs btn-primary sleep__button">Continue</a>
-  </div>
-  
-
+<div class="sleep-wrapper" id="timeout-screen" style="display:none;">
+   <div class="sleep-wrapper__thumb"> 
+      <img class="" src="{{ asset('backend/images/sleep-img.jpg') }}" alt="@lang('Sleep')">
+   </div>
+   <form class='form'>
+      <div class="avatar">
+         <div class="avatar__icon"><img src="{{ asset('backend/images/userlogo.svg') }}" alt="@lang('User')"></div>
+         <div class="avatar__content">
+            <h5 class="avatar__title">User</h5>
+            <button class="sleep__button" type="submit">Continue</button>
+         </div>
+      </div>
+   </form>
+   <div class="sleep-bottom">
+      <div class="sleep-bottom__time">
+         <div id="clockDisplay" class="clock"></div>
+      </div>
+      <div class="sleep-bottom__menu btn-group dropup">
+         <button type="button" class="sleep-bottom-menu__button dropdown-toggle" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+         <img src="{{ asset('backend/images/home.svg') }}" alt="@lang('Home')">
+         </button>
+         <div class="sleep-bottom-dropdown dropdown-menu" x-placement="top-start">
+            <a class="dropdown-item" href="{{ route('admin.dashboard') }}">
+                <span class="dropdown-item__icon"><i class="fas fa-home"></i></span> @lang('Home')
+            </a>
+            <a class="dropdown-item logoutBtn" id="sleep_logoutBtn" href="{{ route('logout') }}">
+                <span class="dropdown-item__icon"><i class="fas fa-sign-out-alt"></i></span> @lang('Log Out')
+            </a>
+         </div> 
+      </div>
+   </div>
 </div>
-
