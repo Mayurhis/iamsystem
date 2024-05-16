1b2d51bc5c10f2c428224222df80c0fec5f25d65
rohitsaini-developer <rohithelpfullinsight@gmail.com>
2024-05-16 09:27:06 +0530

    make changes in design

--- public/backend/js/custom.js
+++ public/backend/js/custom.js
@@ -11,59 +11,6 @@ $(document).ready(function(){
 
 });
 
-$(document).on('change', "#uploadidPic .input-file",function(e){
-	if ($(this).val()) {
-		var filename = $(this).val().split("\\");
-		filename = filename[filename.length-1];
-		$('#uploadidPic .image-name').text(filename);
-	}
-	var files = e.target.files;
-	for (var i = 0; i < files.length; i++) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$('#previewImg1 img').attr('src', e.target.result);
-			// $('#upload_submit').text('Submit');
-			$('#upload_submit').html('Submit <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.29356 2.63052L4.17356 6.51052L0.29356 10.3905C-0.0964404 10.7805 -0.0964404 11.4105 0.29356 11.8005C0.68356 12.1905 1.31356 12.1905 1.70356 11.8005L6.29356 7.21052C6.68356 6.82052 6.68356 6.19052 6.29356 5.80052L1.70356 1.21052C1.31356 0.820519 0.68356 0.820519 0.29356 1.21052C-0.0864404 1.60052 -0.0964404 2.24052 0.29356 2.63052Z" fill="white"/> <path d="M6.87657 2.79434L10.7566 6.67434L6.87657 10.5543C6.48657 10.9443 6.48657 11.5743 6.87657 11.9643C7.26657 12.3543 7.89657 12.3543 8.28657 11.9643L12.8766 7.37434C13.2666 6.98434 13.2666 6.35434 12.8766 5.96434L8.28657 1.37434C7.89657 0.984337 7.26657 0.984337 6.87657 1.37434C6.49657 1.76434 6.48657 2.40434 6.87657 2.79434Z" fill="white"/></svg>');
-			$('#upload_submit').addClass('submit_btn');
-		};
-		reader.readAsDataURL(files[i]);
-	}
-});
-$(document).on('change', "#uploadfacepic .input-file",function(e){
-	if ($(this).val()) {
-		var filename = $(this).val().split("\\");
-		filename = filename[filename.length-1];
-		$('#uploadfacepic .image-name').text(filename);
-	}
-	var files = e.target.files;
-	for (var i = 0; i < files.length; i++) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$('#previewImg2 img').attr('src', e.target.result);
-			$('#upload_submit2').html('Submit <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.29356 2.63052L4.17356 6.51052L0.29356 10.3905C-0.0964404 10.7805 -0.0964404 11.4105 0.29356 11.8005C0.68356 12.1905 1.31356 12.1905 1.70356 11.8005L6.29356 7.21052C6.68356 6.82052 6.68356 6.19052 6.29356 5.80052L1.70356 1.21052C1.31356 0.820519 0.68356 0.820519 0.29356 1.21052C-0.0864404 1.60052 -0.0964404 2.24052 0.29356 2.63052Z" fill="white"/> <path d="M6.87657 2.79434L10.7566 6.67434L6.87657 10.5543C6.48657 10.9443 6.48657 11.5743 6.87657 11.9643C7.26657 12.3543 7.89657 12.3543 8.28657 11.9643L12.8766 7.37434C13.2666 6.98434 13.2666 6.35434 12.8766 5.96434L8.28657 1.37434C7.89657 0.984337 7.26657 0.984337 6.87657 1.37434C6.49657 1.76434 6.48657 2.40434 6.87657 2.79434Z" fill="white"/></svg>');
-			$('#upload_submit2').addClass('submit_btn');
-		};
-		reader.readAsDataURL(files[i]);
-	}
-});
-$(document).on('change', "#uploadaddproof .input-file",function(e){
-	if ($(this).val()) {
-		var filename = $(this).val().split("\\");
-		filename = filename[filename.length-1];
-		$('#uploadaddproof .image-name').text(filename);
-	}
-	var files = e.target.files;
-	for (var i = 0; i < files.length; i++) {
-		var reader = new FileReader();
-		reader.onload = function(e) {
-			$('#previewImg3 img').attr('src', e.target.result);
-			$('#upload_submit3').html('Submit <svg width="14" height="13" viewBox="0 0 14 13" fill="none" xmlns="http://www.w3.org/2000/svg"><path d="M0.29356 2.63052L4.17356 6.51052L0.29356 10.3905C-0.0964404 10.7805 -0.0964404 11.4105 0.29356 11.8005C0.68356 12.1905 1.31356 12.1905 1.70356 11.8005L6.29356 7.21052C6.68356 6.82052 6.68356 6.19052 6.29356 5.80052L1.70356 1.21052C1.31356 0.820519 0.68356 0.820519 0.29356 1.21052C-0.0864404 1.60052 -0.0964404 2.24052 0.29356 2.63052Z" fill="white"/> <path d="M6.87657 2.79434L10.7566 6.67434L6.87657 10.5543C6.48657 10.9443 6.48657 11.5743 6.87657 11.9643C7.26657 12.3543 7.89657 12.3543 8.28657 11.9643L12.8766 7.37434C13.2666 6.98434 13.2666 6.35434 12.8766 5.96434L8.28657 1.37434C7.89657 0.984337 7.26657 0.984337 6.87657 1.37434C6.49657 1.76434 6.48657 2.40434 6.87657 2.79434Z" fill="white"/></svg>');
-			$('#upload_submit3').addClass('submit_btn');
-		};
-		reader.readAsDataURL(files[i]);
-	}
-});
-
 //   change password 
 
 $(document).on('click', '.password_toggle', function() {
